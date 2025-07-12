2ea9c119182665c2326cdca83dbcbe8bc8be5f8b
MD.TAMIM AHMED FAHIM <t.a.fahim24@gmail.com>
2025-07-13 01:21:01 +0600

    Create index.php

---
+++ src/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="./output.css" rel="stylesheet">
+</head>
+<body>
+    <div>
+    <?php
+    //header part start
+    require_once "header.php";
+    //header part end
+    ?>
+
+    
+    <?php 
+    // body part start here
+    require_once "index_body.php";
+    //body part end here
+    ?>
+
+    <?php
+    //footer start
+    require_once "footer.php";
+    //footer stop
+    ?>
+    </div>
+</body>
+</html>
